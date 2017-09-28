6042d73fd7bc3b93f1bd35f19b92aff767ccce9f
ReehcQ <32181145+ReehcQ@users.noreply.github.com>
2017-09-28 18:11:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,7 @@ setup(
 	author='ReehcQ',
 	author_email='czhan888@outlook.com',
 	platform='any',
-	# install_requires=[],
-	# tests_require=[],
-	# packages=['lib'],
 	include_pakage_data=True,
-	# scripts=['lib/stockdownload.py','lib/download.py','lib/mainframe.py'],
 	classifiers=[
 		'Programming Language :: Python :: 2.7',
 		'Natural Language :: English'
